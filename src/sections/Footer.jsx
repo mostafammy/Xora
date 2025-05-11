101b9ec808cadeb7722587617b7aa3cba419c117
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-05-11 11:27:25 +0300

    Edit Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,7 +5,7 @@ const Sections = () => {
       <div className="container py-10">
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="opacity-70">Copyright &copy; JS Mastry </p>
+            <p className="opacity-70">Copyright &copy; Mostafa Yaser </p>
           </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1 cursor-pointer">
